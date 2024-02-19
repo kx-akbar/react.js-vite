e666adcfd0f2f5c74f3cbd1d20c9d223411f8af1
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-02-19 12:00:35 +0500

    the list of students is ready

--- src/Students/List.jsx
+++ src/Students/List.jsx
@@ -3,47 +3,116 @@ import { students } from "./Students";
 import "./ListStyle.css";
 
 function List() {
-  const [data, setdata] = useState(students);
+  const [data, setData] = useState(students);
+  const [selected, setSelected] = useState({
+    select: null,
+  });
 
-  const onDeleteUser = (id) => {
-    const updatedStudents = data.filter((student) => student.id !== id);
-    setdata(updatedStudents);
+  const onRemoveUser = function (usrId) {
+    let ar = data.filter((val) => val.id != usrId);
+    setData(ar);
   };
+
+  const onChanges = function (event) {
+    let { value, placeholder } = event.target;
+    let res = students.filter((val) => {
+      return val[placeholder].toLowerCase().includes(value.toLowerCase());
+    });
+    setData(res);
+  };
+
+  const onUserSelect = function (user) {
+    if (selected.select == null) setSelected({ select: user });
+    else {
+      let res = data.map((val) => {
+        return val.id == selected.select?.id ? selected.select : val;
+      });
+      setSelected({ select: null });
+      setData(res);
+    }
+  };
+
   return (
     <div className="main">
-      <h1 className="h1 fw-bolder">Students List</h1>
-      <table
-        className="table table-dark table-striped"
-        style={{ width: "50%" }}
-      >
-        <thead className="thead-light">
-          <tr className="text-center">
+      <h1 className="fw-bolder">Students List</h1>
+      <div className="d-flex gap-3">
+        <input
+          type="text"
+          className="form-control mb-3"
+          onChange={onChanges}
+          placeholder="name"
+        />
+        <input
+          type="text"
+          className="form-control mb-3"
+          onChange={onChanges}
+          placeholder="status"
+        />
+      </div>
+      <table className="table table-hover table-dark w-75">
+        <thead>
+          <tr>
             <th scope="col">ID</th>
-            <th scope="col">First Name</th>
+            <th scope="col">Name</th>
             <th scope="col">Status</th>
             <th scope="col">Year</th>
+            <th scope="col">Change</th>
             <th scope="col">Delete</th>
-            <th scope="col">Edit</th>
           </tr>
         </thead>
         <tbody>
           {data.map((value) => {
             return (
-              <tr className="text-center" key={value.id}>
+              <tr key={value.id}>
                 <td>{value.id}</td>
-                <td>{value.name}</td>
-                <td>{value.status.toUpperCase()}</td>
+                <td>
+                  {selected.select?.id == value.id ? (
+                    <input
+                      type="text"
+                      className="form-control w-75"
+                      defaultValue={selected.select.name}
+                      onChange={({ target }) =>
+                        setSelected({
+                          select: { ...selected.select, name: target.value },
+                        })
+                      }
+                    />
+                  ) : (
+                    value.name
+                  )}
+                </td>
+                <td>
+                  {selected.select?.id == value.id ? (
+                    <input
+                      type="text"
+                      className="form-control w-75"
+                      defaultValue={selected.select?.status.toUpperCase()}
+                      onChange={({ target }) =>
+                        setSelected({
+                          select: { ...selected.select, status: target.value },
+                        })
+                      }
+                    />
+                  ) : (
+                    value.status.toUpperCase()
+                  )}
+                </td>
                 <td>{value.year}</td>
                 <td>
                   <button
-                    className="btn btn-dark border-light"
-                    onClick={() => onDeleteUser(value.id)}
+                    className="btn border-light bg-danger text-light"
+                    onClick={() => onUserSelect(value)}
                   >
-                    Delete
+                    {selected.select?.id == value.id ? "save" : "change"}
                   </button>
                 </td>
                 <td>
-                  <button className="btn btn-danger border-light">Edit</button>
+                  <button
+                    className="btn border-light btn-dark"
+                    onClick={() => onRemoveUser(value.id)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             );
--- src/Students/Students.js
+++ src/Students/Students.js
@@ -1,11 +1,11 @@
 // students list
 export const students = [
-  { id: 1, name: "Eshmat", status: "cp", year: 2004 },
-  { id: 2, name: "Toshmat", status: "bs", year: 2002 },
+  { id: 1, name: "Toshmat", status: "cp", year: 2004 },
+  { id: 2, name: "Akbar", status: "bs", year: 2002 },
   { id: 3, name: "Eshmat", status: "cc", year: 2001 },
-  { id: 4, name: "Toshmat", status: "cs", year: 2003 },
-  { id: 5, name: "Eshmat", status: "dc", year: 2003 },
-  { id: 6, name: "Toshmat", status: "bs", year: 2002 },
-  { id: 7, name: "Eshmat", status: "cc", year: 2001 },
-  { id: 8, name: "Toshmat", status: "cs", year: 2000 },
+  { id: 4, name: "Qochqor", status: "cs", year: 2003 },
+  { id: 5, name: "Jamshid", status: "dc", year: 2003 },
+  { id: 6, name: "Asadbek", status: "bs", year: 2002 },
+  { id: 7, name: "G'ulom", status: "cc", year: 2001 },
+  { id: 8, name: "Oybek", status: "cs", year: 2000 },
 ];
