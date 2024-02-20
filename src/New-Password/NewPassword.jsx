d8c42aff253d581b65c4b3be3bbde4a92c192665
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-02-21 02:18:13 +0500

    SignUp project is ready

--- src/New-Password/NewPassword.jsx
+++ src/New-Password/NewPassword.jsx
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import "./newPwStyle.css";
 
 function NewPassword() {
+  const [view, setView] = useState({
+    password: false,
+    confirm: false,
+  });
   const [val, setVal] = useState("");
+  const [inpValue, setInpValue] = useState("");
   const [check, setCheck] = useState({
     1: false,
     2: false,
@@ -10,13 +15,46 @@ function NewPassword() {
   });
   const onInputChanges = function (event) {
     const { value, name } = event.target;
-    if (name == "confirm" && value.length > 0) {
+    if (name == "confirm") {
       setVal(value);
     }
     if (name == "password") {
-      let regExp = 
+      setInpValue(value);
+      let reg = /\p{Lu}/gu;
+      let digit = /\p{Nd}/gu;
+
+      // ======================================
+
+      if (value.match(reg)?.length > 0) {
+        setCheck({ 1: true });
+      } else {
+        setCheck({ 1: false });
+      }
+
+      if (value.match(digit)?.length > 0 && check[1] == true) {
+        setCheck({ 1: check[1], 2: true });
+      }
+
+      if (value?.length >= 8 && check[1] == true && check[2] == true) {
+        setCheck({ 1: check[1], 2: check[2], 3: true });
+      }
     }
-    console.log(val);
+  };
+
+  const EyeIconClassPw = view.password
+    ? "fa-regular fa-eye"
+    : "fa-solid fa-eye-slash";
+  const EyeIconClassCf = view.confirm
+    ? "fa-regular fa-eye"
+    : "fa-solid fa-eye-slash";
+
+  const onsubmitBtn = function (e) {
+    e.preventDefault();
+    if (val === inpValue && val.length > 0 && inpValue.length > 0) {
+      alert("Qochqor aka paroliz to'g'ri !");
+    } else {
+      alert("Qochqor aka paroliz Noto'g'ri !");
+    }
   };
   return (
     <div>
@@ -32,11 +70,17 @@ function NewPassword() {
             <div className="inputs">
               <input
                 type="text"
+                value={inpValue}
                 name="password"
+                maxLength={8}
+                autoComplete="off"
                 placeholder="	•	•	•	•	•	•	•	•"
                 onChange={onInputChanges}
               />
-              <i className="fa-solid fa-eye"></i>
+              <i
+                className={EyeIconClassPw}
+                onClick={() => setView({ password: !view.password })}
+              ></i>
             </div>
           </div>
 
@@ -51,10 +95,15 @@ function NewPassword() {
                 type="text"
                 name="confirm"
                 value={val}
+                maxLength={8}
+                autoComplete="off"
                 placeholder="	•	•	•	•	•	•	•	•"
                 onChange={onInputChanges}
               />
-              <i className="fa-solid fa-eye"></i>
+              <i
+                className={EyeIconClassCf}
+                onClick={() => setView({ confirm: !view.confirm })}
+              ></i>
             </div>
           </div>
           <div className="line">
@@ -82,21 +131,55 @@ function NewPassword() {
           </div>
           <div className="test-box">
             <strong>Weak password. Must contain at least;</strong>
-            {/* <i className="fa-regular fa-circle-check"></i> */}
             <p className="password-test">
-              <i className="fa-regular fa-circle-xmark"></i>
+              <i
+                style={
+                  check[1]
+                    ? { color: "green" }
+                    : { color: "rgb(146, 159, 175)" }
+                }
+                className={
+                  check[1]
+                    ? "fa-regular fa-circle-check"
+                    : "fa-regular fa-circle-xmark"
+                }
+              ></i>
               <b>At least 1 uppercase</b>
             </p>
             <p className="password-test">
-              <i className="fa-regular fa-circle-xmark"></i>
+              <i
+                style={
+                  check[2]
+                    ? { color: "green" }
+                    : { color: "rgb(146, 159, 175)" }
+                }
+                className={
+                  check[2]
+                    ? "fa-regular fa-circle-check"
+                    : "fa-regular fa-circle-xmark"
+                }
+              ></i>
               <b>At least 1 number</b>
             </p>
             <p className="password-test">
-              <i className="fa-regular fa-circle-xmark"></i>
+              <i
+                style={
+                  check[3]
+                    ? { color: "green" }
+                    : { color: "rgb(146, 159, 175)" }
+                }
+                className={
+                  check[3]
+                    ? "fa-regular fa-circle-check"
+                    : "fa-regular fa-circle-xmark"
+                }
+              ></i>
               <b>At least 8 characters</b>
             </p>
           </div>
-          <button className="continue-btn">Continue</button>
+          <button className="continue-btn" onClick={onsubmitBtn}>
+            Continue
+          </button>
         </form>
       </main>
     </div>
@@ -104,6 +187,3 @@ function NewPassword() {
 }
 
 export default NewPassword;
-{
-  /* <i class="fa-solid fa-eye-slash"></i> */
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import LogIn from "./LogIn/LogIn";
-// import ResetPassword from "./Reset-Password/ResetPassword.jsx";
-// import EnterCode from "./Enter-Code/EnterCode.jsx";
+import LogIn from "./LogIn/LogIn";
+import ResetPassword from "./Reset-Password/ResetPassword.jsx";
+import EnterCode from "./Enter-Code/EnterCode.jsx";
 import NewPassword from "./New-Password/NewPassword.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <LogIn /> */}
-    {/* <ResetPassword /> */}
+    <LogIn />
+    <ResetPassword />
+    <EnterCode />
     <NewPassword />
-    {/* <EnterCode /> */}
   </React.StrictMode>
 );
