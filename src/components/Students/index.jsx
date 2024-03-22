16406f56ef01d80f4f95546e2528b6cafbd6ff58
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-03-22 23:38:54 +0500

    fetch

--- src/components/Students/index.jsx
+++ src/components/Students/index.jsx
@@ -1,31 +1,47 @@
 import React, { useEffect, useState } from "react";
-// import menu from "../../utils/student.json";
 
 const url = "https://jsonplaceholder.typicode.com/";
 
 function Fetch() {
-  const [todo, setTodo] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+
   useEffect(() => {
     fetch(`${url}users/`)
       .then((res) => res.json())
-      .then((res) => setTodo(res));
+      .then((res) => setUsers(res));
   }, []);
+
+  const onSelect = (id) => {
+    fetch(`${url}users/${id}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => setUser(res));
+  };
+
   return (
     <div style={{ display: "flex", justifyContent: "space-evenly" }}>
       <div style={{ margin: "15px" }}>
         <h1>Users</h1>
-        {todo.length &&
-          todo.map((todos) => {
-            return (
-              <h1 key={todos.id}>
-                {todos.id} - {todos.name}
-                <button>select</button>
-              </h1>
-            );
-          })}
+        {users.map((user) => {
+          return (
+            <h1 key={user.id}>
+              {user.id} - {user.name}
+              <button onClick={() => onSelect(user.id)}>select</button>
+            </h1>
+          );
+        })}
       </div>
       <div>
-        <h1>Selected User</h1>
+        <h1>Selected User {users.id}</h1>
+        <div>
+          <h5>
+            Name: {user.name} {user.surname}
+          </h5>
+          <h4>Email: {user.email}</h4>
+          <h4>Phone: {user.phone}</h4>
+        </div>
       </div>
     </div>
   );
